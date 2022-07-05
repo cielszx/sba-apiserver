4c5be794718ade6a94b940c4426d9df2f9595079
suzhenxing <cielszx@163.com>
2022-07-05 18:51:54 +0800

    make

---
+++ src/main/java/com/axess/smartbankapi/controller/RedeemptionHistoryController.java
@@ -0,0 +1,53 @@
+package com.axess.smartbankapi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.axess.smartbankapi.config.restapi.ApiSuccessResponse;
+import com.axess.smartbankapi.dto.UserRedeemptionHistoryDto;
+import com.axess.smartbankapi.exception.RecordExistException;
+import com.axess.smartbankapi.exception.RecordNotCreatedException;
+import com.axess.smartbankapi.exception.RecordNotFoundException;
+import com.axess.smartbankapi.service.RedeemptionHistoryService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/history")
+@Slf4j
+public class RedeemptionHistoryController {
+
+	
+	@Autowired
+	private RedeemptionHistoryService historyService;
+	
+	
+	@PostMapping("/")
+	public ResponseEntity<?> saveHistory(@RequestBody UserRedeemptionHistoryDto historyDto) throws RecordNotFoundException, RecordExistException, RecordNotCreatedException {
+		
+		log.info("Request for redeemption history received");
+		ApiSuccessResponse response = new ApiSuccessResponse();
+
+		response.setMessage("Successfully added to history. ");
+		response.setHttpStatus(String.valueOf(HttpStatus.CREATED));
+		response.setHttpStatusCode(201);
+		response.setBody(historyService.saveHistory(historyDto));
+		response.setError(false);
+		response.setSuccess(true);
+
+		return ResponseEntity.status(HttpStatus.OK).header("status", String.valueOf(HttpStatus.OK))
+				.body(response);
+
+
+	}
+
+	
+	
+}
---
+++ src/main/java/com/axess/smartbankapi/controller/RewardsCatalogueController.java
@@ -0,0 +1,73 @@
+package com.axess.smartbankapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.axess.smartbankapi.config.restapi.ApiSuccessResponse;
+import com.axess.smartbankapi.exception.RecordNotFoundException;
+import com.axess.smartbankapi.model.RewardsCatalogue;
+import com.axess.smartbankapi.service.RewardCatalogueService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/catalogue")
+@CrossOrigin
+@Slf4j
+public class RewardsCatalogueController {
+	
+	@Autowired
+	private RewardCatalogueService rcService;
+	
+	@GetMapping("/")
+	public ResponseEntity<?> getAll() throws RecordNotFoundException {
+		
+		log.info("Request for catalogue received");
+		ApiSuccessResponse response = new ApiSuccessResponse();
+
+		List<RewardsCatalogue> rc = this.rcService.getAll();
+
+		response.setMessage("Total catalogue count - " + rc.size());
+		response.setHttpStatus(String.valueOf(HttpStatus.FOUND));
+		response.setHttpStatusCode(302);
+		response.setBody(rc);
+		response.setError(false);
+		response.setSuccess(true);
+
+		return ResponseEntity.status(HttpStatus.OK).header("status", String.valueOf(HttpStatus.OK))
+				.body(response);
+
+
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getById(@PathVariable long id) throws RecordNotFoundException {
+		
+		log.info("Request for catalogue having id received: "+id);
+		ApiSuccessResponse response = new ApiSuccessResponse();
+
+		RewardsCatalogue rc = this.rcService.getById(id);
+
+		response.setMessage("Successfully Found. ");
+		response.setHttpStatus(String.valueOf(HttpStatus.FOUND));
+		response.setHttpStatusCode(302);
+		response.setBody(rc);
+		response.setError(false);
+		response.setSuccess(true);
+
+		return ResponseEntity.status(HttpStatus.OK).header("status", String.valueOf(HttpStatus.OK))
+				.body(response);
+
+
+	}
+
+
+}
